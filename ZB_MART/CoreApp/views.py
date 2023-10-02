7ca2c9e0d3054184057ec6d3d209edfeca879256
sandy-exe <santhoshkumar555001@gmail.com>
2023-10-02 22:16:57 +0530

    completed

--- ZB_MART/CoreApp/views.py
+++ ZB_MART/CoreApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy,reverse
 from django.views import generic
 from django.views.generic import TemplateView,ListView,DetailView, View
 from django.contrib.auth.forms import UserCreationForm
@@ -10,6 +10,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from .models import Item,Category,OrderItem,Order
 from django.shortcuts import redirect, get_object_or_404
 import time
+
 
 class HomeView(ListView):
     template_name = "index.html"
@@ -60,24 +61,27 @@ def add_to_cart(request, slug):
     )
     print(order_item)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
+    print(request.META.get('HTTP_REFERER'))
     if order_qs.exists():
         order = order_qs[0]
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
             messages.info(request, "Item qty was updated.")
-            return redirect("CoreApp:cart")
         else:
             order.items.add(order_item)
             messages.info(request, "Item was added to your cart.")
-            return redirect("CoreApp:cart")
     else:
         ordered_date = time.now()
         order = Order.objects.create(
             user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
         messages.info(request, "Item was added to your cart.")
-    return redirect("CoreApp:cart")
+    previous_url = request.META.get('HTTP_REFERER')
+    if previous_url.endswith(reverse('CoreApp:cart')):
+        return redirect("CoreApp:cart")
+    # Redirect the user back to the previous page
+    return redirect(previous_url)
 
 
 @login_required
@@ -139,10 +143,6 @@ def remove_single_item_from_cart(request, slug):
         # add a message saying the user dosent have an order
         messages.info(request, "u don't have an active order.")
         return redirect("CoreApp:product", slug=slug)
-    return redirect("CoreApp:product", slug=slug)
-
-
-
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
